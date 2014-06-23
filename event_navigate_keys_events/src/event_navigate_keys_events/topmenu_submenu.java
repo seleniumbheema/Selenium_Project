7759c6d16c9c25688b79f93641a8a271e36466c5
seleniumbheema <akbheema@gmail.com>
2014-06-23 23:47:09 +0100

    many changes isdisplayed

--- event_navigate_keys_events/src/event_navigate_keys_events/topmenu_submenu.java
+++ event_navigate_keys_events/src/event_navigate_keys_events/topmenu_submenu.java
@@ -1,5 +1,7 @@
 package event_navigate_keys_events;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -9,7 +11,7 @@ import org.openqa.selenium.firefox.internal.ProfilesIni;
 
 public class topmenu_submenu {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		
 		ProfilesIni allprof = new ProfilesIni();
@@ -18,10 +20,19 @@ public class topmenu_submenu {
 		WebDriver driv = new FirefoxDriver(myprof);
 		
 		driv.get("http://www.google.com");
-	driv.findElement(By.xpath("//*[@id='gs_htif0']")).sendKeys("menus");
-	driv.findElement(By.xpath("//*[@id='gs_htif0']")).sendKeys(Keys.ENTER);
+	System.out.println
+	(driv.findElement(By.xpath("html/body/div[1]/div[2]/div[1]/div[1]/div[3]/div/div/div/form/fieldset[2]/div/div/div/table/tbody/tr/td[2]"))
+			.isDisplayed());
+	driv.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+driv.findElement(By.xpath("html/body/div[1]/div[2]/div[1]/div[1]/div[3]/div/div/div/form/fieldset[2]/div/div/div/table/tbody/tr/td[2]")).sendKeys("future");
+driv.findElement(By.xpath("html/body/div[1]/div[2]/div[1]/div[1]/div[3]/div/div/div/form/fieldset[2]/div/div/div/table/tbody/tr/td[2]")).sendKeys(Keys.ENTER);
+
 	
-	
+	driv.navigate().back();
+	Thread.sleep(1000L);
+	driv.navigate().forward();
+	Thread.sleep(1000L);
+//	driv.quit();
 	
 		
 		
